4776ec184ae178ff8222d464edcde02734bfa184
gioimtg2003 <conggioi.pro264@gmail.com>
2024-01-01 15:18:10 +0700

    update host mysql

--- configs/config.php
+++ configs/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST',"localhost");
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST',"database-1.cto4a8qusgp1.ap-southeast-1.rds.amazonaws.com");
+define('DB_USER','admin');
+define('DB_PASS','conggioi123');
 define('DB_NAME','quanlychuoicuahang');
 define('LIMIT_PAGE', 12);
  ?>
