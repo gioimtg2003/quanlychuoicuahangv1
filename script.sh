854647c36190932396032aae528399391f898f3c
gioimtg2003 <conggioi.pro264@gmail.com>
2024-01-17 01:57:37 +0700

    update script

--- script.sh
+++ script.sh
@@ -2,5 +2,22 @@ docker compose build
 docker compose up -d 
 echo "RUN SUCCESS"
 docker ps
+sleep 2
+echo "RUN SCRIPT SQL ===========>"
+docker exec -it mysql_database bash -c 'mysql -u root --password="$MYSQL_ROOT_PASSWORD" quanlychuoicuahang < /script_sql/database.sql'
+echo "RUN SCRIPT SQL SUCCESS ===========>"
+
 sleep 1
-docker exec -it mysql_database bash -c 'mysql -u root --password="my-password" quanlychuoicuahang < /script_sql/database.sql'
+docker images
+#tag
+echo "TAG IMAGE ==============>"
+docker tag quanlychuoicuahang-php:latest gioimtg2003/project-image-php:v1.1
+docker tag quanlychuoicuahang-nginx:latest gioimtg2003/project-image-nginx:v1.1
+docker tag quanlychuoicuahang-mysql:latest gioimtg2003/project-image-mysql:v1.1
+
+docker push gioimtg2003/project-image-php:v1.1
+docker push gioimtg2003/project-image-ngix:v1.1
+docker push gioimtg2003/project-image-mysql:v1.1
+
+
+
